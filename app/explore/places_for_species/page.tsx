99286e27d96f0f9a0530ced222b617f0c652a037
Greg <gregorylow@yahoo.com>
2026-02-01 10:34:36 -0500

    added placeholder in places for species page

---
+++ app/explore/places_for_species/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function PlacesForSpeciesPage() {
+  return (
+    <div style={{ padding: '40px' }}>
+      <h1>Specific Species Tool</h1>
+      <p>Building this next...</p>
+    </div>
+  )
+}
